062a7d638bb4ec1ff0dbf13a492f99ca03fe2de4
krjovan <jovankron@gmail.com>
2023-01-04 12:18:33 +0100

    Created separate functional component to be directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -28,16 +27,9 @@ const App = () => {
       title: "mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
     }
-  ]
-  ;
+  ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(category => (
-        <CategoryItem key={category.id} category={category}/>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
