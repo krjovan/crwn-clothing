dc976cc3a2bba33db0bd8e253465b7f9810c3bcf
krjovan <jovankron@gmail.com>
2023-01-05 11:48:17 +0100

    Added basic navbar with index on the home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
+
+const Navigation = ()=> {
+  return (
+   <div>
+    <div>
+      <h1>I am the navigation bar</h1>
+    </div>
+    <Outlet />
+   </div>
+  );
+}
+
+const Shop = ()=> {
+  return <h1>I am the Shop page</h1>;
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 };
