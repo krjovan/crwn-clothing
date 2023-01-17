55f95a75a503f3ab3bb22d5a151254735abe7f5f
unknown <jovankron@gmail.com>
2023-01-17 13:47:08 +0100

    Set up Sign up with email and password form and used useState to track changes

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.util";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
@@ -12,6 +14,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google popup
             </button>
+            <SignUpForm />
         </div>
     )
 }
