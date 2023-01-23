4bd4bba03813dca3ef94d824bf4ec8757f52a3a4
krjovan <jovankron@gmail.com>
2023-01-23 11:01:14 +0100

    Created Button component with custom styling depending on buttonType

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.util";
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
     displayName: '',
@@ -49,8 +51,9 @@ const SignUpForm = () => {
     };
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Display name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
@@ -59,7 +62,7 @@ const SignUpForm = () => {
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
 
                 <FormInput label="Confirm password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
