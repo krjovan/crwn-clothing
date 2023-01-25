b4477d30020c57e60bbddca2d3ad6f330af4a9c1
krjovan <jovankron@gmail.com>
2023-01-25 12:53:52 +0100

    Finished Sign-in and Authentication component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -30,7 +30,16 @@ const SignInForm = () => {
             console.log(response);
             resetFormFields();
         } catch (error) {
-
+            switch (error.code) {
+                case "auth/wrong-password":
+                    alert('incorrect password for email');
+                    break;
+                case "auth/user-not-found":
+                    alert('no user associated with this email');
+                    break;
+                default:
+                    console.log(error);
+            }
         }
 
     }
@@ -50,7 +59,7 @@ const SignInForm = () => {
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
+                    <Button type="button" buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
         </div>
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,12 +1,10 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.util";
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import './authentication.styles.scss';
 
 const Authentication = () => {
     return (
-        <div>
-            <h1>Sign in page</h1>
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm />
         </div>
