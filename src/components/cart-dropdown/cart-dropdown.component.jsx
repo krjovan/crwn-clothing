e18be87ccf46af2f6bcdd21180a9c373685960b9
krjovan <jovankron@gmail.com>
2023-02-08 11:38:20 +0100

    Added Cart and CartDropdown components

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import Button from '../button/button.component';
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items' />
+            <Button>GO TO CHECK OUT</Button>
+        </div>
+    )
+}
+
+export default CartDropdown;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.util';
@@ -29,7 +32,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }      
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
