fc4805290e2533ed98033ef9aa29e727b47568cc
krjovan <jovankron@gmail.com>
2023-02-15 10:38:03 +0100

    Added Checkout page and did basic display with increment of quantity function

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import Button from '../button/button.component';
+import { useNavigate } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
@@ -6,12 +7,18 @@ import { useContext } from 'react';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
-            <Button>GO TO CHECK OUT</Button>
+            <Button onClick={goToCheckoutHandler} >CHECKOUT</Button>
         </div>
     )
 }
