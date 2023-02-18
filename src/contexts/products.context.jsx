350ace89281ac049a425816da393dcdf1227a9f3
krjovan <jovankron@gmail.com>
2023-02-18 10:36:00 +0100

    Imported SHOP_DATA to Firestore with useEffect

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 import SHOP_DATA from "../shop-data.js";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.util.js";
 
 export const ProductsContext = createContext({
     products: []
@@ -9,6 +9,7 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState([]);
+
     const value = { products };
     return (
         <ProductsContext.Provider value={value}> {children} </ProductsContext.Provider>
