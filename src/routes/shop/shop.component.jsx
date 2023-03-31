d5b7bb2b6f27dde199f3e526b4b8fbe94dc8cb99
krjovan <jovankron@gmail.com>
2023-03-31 13:03:31 +0200

    Reformating categories fetch function to be done in Selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.util';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 import './shop.styles.scss';
 
 const Shop = () => {
@@ -12,8 +12,8 @@ const Shop = () => {
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray = await getCategoriesAndDocuments();
+            dispatch(setCategories(categoriesArray));
         };
         
         getCategoriesMap();
