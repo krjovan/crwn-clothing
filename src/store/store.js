7ff0d620ea7019f4e991504184d87502736fb981
krjovan <jovankron@gmail.com>
2023-04-02 12:40:01 +0200

    Made custom logger middleware to corect order of logs

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,24 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currnetState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+
+}
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
