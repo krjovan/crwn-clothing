178d6d9334d5210c32c18969587f2c119718fa8b
krjovan <jovankron@gmail.com>
2023-04-06 12:23:33 +0200

    Added Redux-persist for local-storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 //import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -18,8 +20,18 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
